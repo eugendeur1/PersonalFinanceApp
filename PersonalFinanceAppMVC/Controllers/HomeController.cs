0edf108af518c9fea16556b31a73eb4e3ea57b4e
Eugen Deur <eugendeur556@gmail.com>
2024-02-08 13:14:56 +0100

    Stvorio sam vezu između html i c# git log

--- PersonalFinanceAppMVC/Controllers/HomeController.cs
+++ PersonalFinanceAppMVC/Controllers/HomeController.cs
@@ -28,10 +28,55 @@ namespace PersonalFinanceAppMVC.Controllers
         public IActionResult Tomo() { 
             return View();
         }
+
         public IActionResult Card()
         {
             return View();
         }
+        public IActionResult Placanje()
+        {
+            return View();
+        }
+        public IActionResult Statistika() {
+            return View();
+        }
+        public IActionResult Izvjesce()
+        {
+            return View();
+        }
+        public IActionResult Proracun()
+        {
+            return View();
+        }
+        public IActionResult Racun()
+        {
+            return View();
+        }
+        public IActionResult Prihod()
+        {
+            return View();
+        }
+        public IActionResult Dugovi()
+        {
+            return View();
+        }
+        public IActionResult Racuni()
+        {
+            return View();
+        }
+        public IActionResult Ulaganja()
+        {
+            return View();
+        }
+        public IActionResult Postavke()
+        {
+            return View();
+        }
+        public IActionResult Info()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
