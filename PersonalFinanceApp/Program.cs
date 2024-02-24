ada3d1f440f203036fad4e7ec1d351df96819bef
Tomislav Tolj <tomislav.tolj@aircash.eu>
2024-02-24 15:21:15 +0100

    PER-9 just a comment

--- PersonalFinanceApp/Program.cs
+++ PersonalFinanceApp/Program.cs
@@ -14,6 +14,7 @@ namespace PersonalFinanceApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //comment
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
